af7cce960cd7e2c7a1cb224aed3e618a2d928750
Wynston <houyewei@gmail.com>
2023-11-13 17:21:30 +0800

    cloudflare domain ip update

--- ddns.py
+++ ddns.py
@@ -22,10 +22,16 @@ def set_ip(current_ip: str):
     """
     zone_id = os.environ.get("ZONE_ID")
     record_id = os.environ.get("RECORD_ID")
+    spf_record_id = os.environ.get("SPF_RECORD_ID")
     url = (
         "https://api.cloudflare.com/client/v4/zones/%(zone_id)s/dns_records/%(record_id)s"
         % {"zone_id": zone_id, "record_id": record_id}
     )
+    spfurl = (
+        "https://api.cloudflare.com/client/v4/zones/%(zone_id)s/dns_records/%(record_id)s"
+        % {"zone_id": zone_id, "record_id": spf_record_id}
+    )
+
 
     api_key = os.environ.get("API_KEY")
     user_email = os.environ.get("USER_EMAIL")
@@ -36,11 +42,14 @@ def set_ip(current_ip: str):
         "X-Auth-Key": api_key,
         "Content-Type": "application/json",
     }
-    #print(url)
     payload = {"type": "A", "name": record_name, "content": current_ip}
     response = requests.put(url, headers=headers, data=json.dumps(payload))
     print(response.status_code)
-
+    netvigator_ip_range = os.environ.get("NETVIGATOR_IP_RANGE")
+    if netvigator_ip_range is not None:
+        spfpayload = {"type": "TXT", "name": record_name, "content": "v=spf1 ip4:" + current_ip + " " + netvigator_ip_range}
+        spfresponse = requests.put(spfurl, headers=headers, data=json.dumps(spfpayload))
+        print(spfresponse)
 
 def main():
     current_ip = get_ip()
--- ddns.sh
+++ ddns.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
 BASEDIR=$(dirname "$0")
 source $BASEDIR/.env
+DATE=$(date)
 
 $BASEDIR/venv/bin/python $BASEDIR/ddns.py
+
